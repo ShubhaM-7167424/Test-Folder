737b8b3a9c24ce7b242e1402d30c0ad1b50a1ad5
shubhamsingh7649289@gmail.com <shubhamsingh7167424@gmail.com>
2024-01-17 12:16:24 +0530

    'some fix'

---
+++ Folder15/index.js
@@ -0,0 +1,23 @@
+console.log('my name is shubham');
+console.log('my name is shubham');
+console.log('my name is shubham');
+
+console.log('I am live');
+console.log('I am live');
+console.log('I am live');
+console.log('I am live');
+console.log('I am live');
+console.log('I am live');
+console.log('I am live');
+console.log('I am live');
+console.log('I am live');
+console.log('I am live');
+console.log('I am live');
+console.log('I am live');
+console.log('I am live');
+
+console.log('I am working in a college');
+console.log('I am working in a college');
+console.log('I am working in a college');
+console.log('I am working in a college');
+console.log('I am working in a college');
